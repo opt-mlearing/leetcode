1783943f146c24f0e40801025bf6a6e41cabcf17
caogaoli <caogaoli@baidu.com>
2022-03-11 22:20:24 +0800

    binary search tree.

---
+++ src/main/java/leetcode/Solution515.java
@@ -0,0 +1,62 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * 在每个树行中找最大值
+ * https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/
+ */
+public class Solution515 {
+
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        bfs(root, list);
+        return list;
+    }
+
+    private void bfs(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> deque = new ArrayDeque<TreeNode>();
+        deque.offer(root);
+        while (!deque.isEmpty()) {
+            int len = deque.size();
+            int maxVal = Integer.MIN_VALUE;
+            for (int i = 0; i < len; ++i) {
+                TreeNode node = deque.poll();
+                maxVal = Math.max(maxVal, node.val);
+                if (node.left != null) {
+                    deque.offer(node.left);
+                }
+                if (node.right != null) {
+                    deque.offer(node.right);
+                }
+            }
+            list.add(maxVal);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
