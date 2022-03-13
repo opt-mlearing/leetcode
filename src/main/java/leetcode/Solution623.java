42852367b429f9c5bf18971ac17a23f12f9be2f1
caogaoli <caogaoli@baidu.com>
2022-03-13 22:25:40 +0800

    binary search tree.

---
+++ src/main/java/leetcode/Solution623.java
@@ -0,0 +1,56 @@
+package leetcode;
+
+/**
+ * 在二叉树中增加一行
+ * https://leetcode-cn.com/problems/add-one-row-to-tree/
+ */
+public class Solution623 {
+
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (depth == 1) {
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        }
+        dfs(root, val, depth, 1);
+        return root;
+    }
+
+    private void dfs(TreeNode node, int val, int depth, int currDepth) {
+        if (node == null) {
+            return;
+        }
+        if (depth == currDepth + 1) {
+            TreeNode tmp = node.left;
+            node.left = new TreeNode(val);
+            node.left.left = tmp;
+            tmp = node.right;
+            node.right = new TreeNode(val);
+            node.right.right = tmp;
+            return;
+        } else {
+            dfs(node.left, val, depth, currDepth + 1);
+            dfs(node.right, val, depth, currDepth + 1);
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
