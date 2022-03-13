2e68db9288b38489333972dd7695e604aab20f1e
caogaoli <caogaoli@baidu.com>
2022-03-14 01:03:43 +0800

    double pointer

---
+++ src/main/java/leetcode/Solution88.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * 合并两个有序数组.
+ * https://leetcode-cn.com/problems/merge-sorted-array/
+ */
+public class Solution88 {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        while (tail >= 0) {
+            if (p1 == -1) {
+                nums1[tail--] = nums2[p2--];
+            } else if (p2 == -1) {
+                nums1[tail--] = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                nums1[tail--] = nums1[p1--];
+            } else {
+                nums1[tail--] = nums2[p2--];
+            }
+        }
+    }
+
+}
