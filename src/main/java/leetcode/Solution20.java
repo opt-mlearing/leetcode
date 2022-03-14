7841d49f5c818e7f33686bca2830318ecf4eb6f9
caogaoli <caogaoli@baidu.com>
2022-03-15 00:28:12 +0800

    2022-03-16

---
+++ src/main/java/leetcode/Solution20.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * 有效的括号
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ */
+public class Solution20 {
+
+    public boolean isValid(String s) {
+        int len = s.length();
+        if (len % 2 != 0) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        Deque<Character> deque = new LinkedList<Character>();
+        for (int i = 0; i < s.length(); ++i) {
+            char tmp = s.charAt(i);
+            if (map.containsKey(tmp)) {
+                // 存进来的是右括号类型.
+                if (deque.isEmpty() || deque.peek() != map.get(tmp)) {
+                    return false;
+                }
+                deque.pop();
+            } else {
+                // 先存左括号类型.
+                // 存进来的是左括号类型.
+                deque.push(tmp);
+            }
+        }
+        return deque.isEmpty();
+    }
+
+}
---
+++ src/main/java/leetcode/Solution215.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 数组中的第K个最大元素
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+ */
+public class Solution215 {
+
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length < k) {
+            return Integer.MIN_VALUE;
+        }
+        Deque<Integer> deque = new LinkedList<Integer>();
+        Deque<Integer> tmpDeque = new LinkedList<Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            int tmp = nums[i];
+            while (!deque.isEmpty() && deque.peek() > nums[i]) {
+                tmpDeque.push(deque.pop());
+            }
+            deque.push(nums[i]);
+            while (!tmpDeque.isEmpty()) {
+                deque.push(tmpDeque.pop());
+            }
+        }
+        while (k > 1) {
+            deque.pop();
+            k--;
+        }
+        return deque.pop();
+    }
+
+}
