2351fc5953163231e16e15741c62f6804e7df6f0
caogaoli <caogaoli@baidu.com>
2022-03-17 02:19:29 +0800

    double pointer.

---
+++ src/main/java/leetcode/Solution287.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 寻找重复数
+ * https://leetcode-cn.com/problems/find-the-duplicate-number/
+ */
+public class Solution287 {
+
+    public int findDuplicate(int[] nums) {
+        int left = 1;
+        int right = nums.length - 1;
+        int res = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            int count = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                if (nums[i] <= mid) {
+                    count++;
+                }
+            }
+            if (count <= mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+                res = mid;
+            }
+        }
+        return res;
+    }
+
+}
