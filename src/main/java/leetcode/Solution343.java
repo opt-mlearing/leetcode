df800d6f1f5c331fb3d72ce2718a22d02e3eb5e0
caogaoli <caogaoli@baidu.com>
2022-03-28 02:39:59 +0800

    dynamic planning.

---
+++ src/main/java/leetcode/Solution343.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/**
+ * 整数拆分
+ * https://leetcode-cn.com/problems/integer-break/
+ */
+public class Solution343 {
+
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j < i; ++j) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+
+}
