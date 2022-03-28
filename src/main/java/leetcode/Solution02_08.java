3405eb3f1b3ddbd860401706fe15636fcb1391ca
caogaoli <caogaoli@baidu.com>
2022-03-29 01:10:32 +0800

    程序员面试金典（第 6 版)

---
+++ src/main/java/leetcode/Solution02_08.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class Solution02_08 {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while (head != fast) {
+                    head = head.next;
+                    fast = fast.next;
+                }
+                return head;
+            }
+        }
+        return null;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
