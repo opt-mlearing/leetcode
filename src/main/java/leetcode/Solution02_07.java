0be2ba71d9bf0634c21cab9870d520c6783a0660
caogaoli <caogaoli@baidu.com>
2022-03-29 00:57:37 +0800

    程序员面试金典（第 6 版)

---
+++ src/main/java/leetcode/Solution02_07.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * 面试题 02.07. 链表相交
+ * https://leetcode-cn.com/problems/intersection-of-two-linked-lists-lcci/
+ */
+public class Solution02_07 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+}
