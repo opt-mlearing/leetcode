dc67840a6daeaa8b3d02bda1f5b7ac7c65acb569
caogaoli <caogaoli@baidu.com>
2022-03-29 00:46:49 +0800

    程序员面试金典（第 6 版)

---
+++ src/main/java/leetcode/Solution02_06.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+/**
+ * 面试题 02.06. 回文链表
+ * https://leetcode-cn.com/problems/palindrome-linked-list-lcci/
+ */
+public class Solution02_06 {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode leftEnd = findMid(head);
+        ListNode right = reverse(leftEnd.next);
+        while (right != null) {
+            if (head.val != right.val) {
+                return false;
+            }
+            head = head.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode node) {
+        ListNode pre = null;
+        while (node != null) {
+            ListNode tmp = node.next;
+            node.next = pre;
+            pre = node;
+            node = tmp;
+        }
+        return pre;
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
