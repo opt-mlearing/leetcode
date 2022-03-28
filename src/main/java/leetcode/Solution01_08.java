41742ecbe0e52ad4c95fc7dbb3716b3a1f730fe3
caogaoli <caogaoli@baidu.com>
2022-03-28 21:20:13 +0800

    程序员面试金典（第 6 版)

---
+++ src/main/java/leetcode/Solution01_08.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * 面试题 01.08. 零矩阵
+ * https://leetcode-cn.com/problems/zero-matrix-lcci/
+ */
+public class Solution01_08 {
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] column = new boolean[n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (row[i] || column[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+}
