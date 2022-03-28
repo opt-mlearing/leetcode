da653de2a2350de0574eccd51e7b436f39a55d83
caogaoli <caogaoli@baidu.com>
2022-03-29 01:28:45 +0800

    程序员面试金典（第 6 版)

---
+++ src/main/java/leetcode/Solution04_02.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 面试题 04.02. 最小高度树
+ * https://leetcode-cn.com/problems/minimum-height-tree-lcci/
+ */
+public class Solution04_02 {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        Arrays.sort(nums);
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = (left + right) >> 1;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, left, mid - 1);
+        root.right = dfs(nums, mid + 1, right);
+        return root;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
