e5d40e1ff1b7fa6e5406029083b78316a285556f
caogaoli <caogaoli@baidu.com>
2022-03-28 12:17:05 +0800

    dynamic planning.

--- src/main/java/leetcode/Solution01_07.java
+++
@@ -1,2 +0,0 @@
-package leetcode;public class Solution01_07 {
-}
---
+++ src/main/java/leetcode/Solution376.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+/**
+ * 摆动序列
+ * https://leetcode-cn.com/problems/wiggle-subsequence/
+ */
+public class Solution376 {
+
+    public int wiggleMaxLength(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int[] up = new int[n];
+        up[0] = 1;
+        int[] down = new int[n];
+        down[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > nums[i - 1]) {
+                up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+                down[i] = down[i - 1];
+            } else if (nums[i] < nums[i - 1]) {
+                up[i] = up[i - 1];
+                down[i] = Math.max(up[i - 1] + 1, down[i - 1]);
+            } else {
+                up[i] = up[i - 1];
+                down[i] = down[i - 1];
+            }
+        }
+        return Math.max(up[n - 1], down[n - 1]);
+    }
+
+    public int wiggleMaxLength_1(int[] nums) {
+        int up = 1;
+        int down = 1;
+        int n = nums.length;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] > nums[i - 1]) {
+                up = down + 1;
+            } else if (nums[i] < nums[i - 1]) {
+                down = up + 1;
+            }
+        }
+        return n > 0 ? Math.max(up, down) : 0;
+    }
+
+}
