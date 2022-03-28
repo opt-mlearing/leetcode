472327d5e0ac1f9bfa30f594e49293719d5dcb74
caogaoli <caogaoli@baidu.com>
2022-03-28 21:26:17 +0800

    程序员面试金典（第 6 版)

---
+++ src/main/java/leetcode/Solution01_09.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * 面试题 01.09. 字符串轮转
+ * https://leetcode-cn.com/problems/string-rotation-lcci/submissions/
+ */
+public class Solution01_09 {
+
+    public boolean isFlipedString(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        return (s2 + s2).contains(s1);
+    }
+
+}
