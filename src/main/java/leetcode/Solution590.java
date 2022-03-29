3b2338b3359bda24bb753d2421102645c40d15f5
caogaoli <caogaoli@baidu.com>
2022-03-29 12:19:29 +0800

    tree

---
+++ src/main/java/leetcode/Solution590.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * N 叉树的后序遍历
+ * https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal/
+ */
+public class Solution590 {
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<Integer>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(Node node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        for (Node subNode : node.children) {
+            dfs(subNode, res);
+        }
+        res.add(node.val);
+    }
+
+    private static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+}
