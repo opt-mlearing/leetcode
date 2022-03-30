310e9f57cef6cf1c93f3358b6e6f10e2d110b807
caogaoli <caogaoli@baidu.com>
2022-03-31 00:01:16 +0800

    tree

---
+++ src/main/java/leetcode/Solution865.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+/**
+ * 具有所有最深节点的最小子树
+ * https://leetcode-cn.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+ */
+public class Solution865 {
+
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        int left = findDepth(root.left);
+        int right = findDepth(root.right);
+        if (left == right) {
+            return root;
+        }
+        if (left > right) {
+            return subtreeWithAllDeepest(root.left);
+        }
+        return subtreeWithAllDeepest(root.right);
+    }
+
+    private int findDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(findDepth(root.left), findDepth(root.right));
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
