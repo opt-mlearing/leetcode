12e39b4857877d9470979961157c82eb80d14ae1
caogaoli <caogaoli@baidu.com>
2022-03-31 18:46:01 +0800

    tree.

---
+++ src/main/java/leetcode/Solution971.java
@@ -0,0 +1,64 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 翻转二叉树以匹配先序遍历
+ * https://leetcode-cn.com/problems/flip-binary-tree-to-match-preorder-traversal/
+ */
+public class Solution971 {
+
+    private List<Integer> flipped;
+    private int index;
+
+    public List<Integer> flipMatchVoyage(TreeNode root, int[] voyage) {
+        this.flipped = new ArrayList();
+        this.index = 0;
+        dfs(root, voyage);
+        if (flipped.size() > 0 && flipped.get(0) == -1) {
+            flipped.clear();
+            flipped.add(-1);
+        }
+        return flipped;
+    }
+
+    public void dfs(TreeNode node, int[] voyage) {
+        if (node != null) {
+            if (node.val != voyage[index++]) {
+                flipped.clear();
+                flipped.add(-1);
+                return;
+            }
+            // 如果发现root的left&right的val与 voyage对应不上，则旋转一下.
+            if (index < voyage.length && node.left != null && node.left.val != voyage[index]) {
+                flipped.add(node.val);
+                dfs(node.right, voyage);
+                dfs(node.left, voyage);
+            } else {
+                dfs(node.left, voyage);
+                dfs(node.right, voyage);
+            }
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
