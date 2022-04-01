1331f6e1b62caaf0cf2a552966ae35e790987519
caogaoli <caogaoli@baidu.com>
2022-04-02 02:40:52 +0800

    sort array

---
+++ src/main/java/leetcode/Solution162.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+/**
+ * 寻找峰值
+ * https://leetcode-cn.com/problems/find-peak-element/
+ */
+public class Solution162 {
+
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
+}
