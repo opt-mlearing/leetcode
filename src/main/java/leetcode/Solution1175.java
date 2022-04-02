9b0e6f2ad369337a750453d2bd2c31f8f8c88023
caogaoli <caogaoli@baidu.com>
2022-04-02 11:36:59 +0800

    prime number

---
+++ src/main/java/leetcode/Solution1175.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 质数排列
+ * https://leetcode-cn.com/problems/prime-arrangements/
+ */
+public class Solution1175 {
+
+    // 为什么是1000000007?
+    // 1000000007 是最小的十位质数。模1000000007，可以保证值永远在int的范围内.
+    private static long N = (long) 1e9 + 7;
+
+    public int numPrimeArrangements(int n) {
+        int primeSize = primeCount(n + 1);
+        long res = 1;
+        for (int i = 1; i <= primeSize; ++i) {
+            res = (res % N) * i;
+        }
+        for (int i = 1; i <= n - primeSize; ++i) {
+            res = (res % N) * i;
+        }
+        return (int) (res % N);
+    }
+
+    // 计算小于等于n中,质数的个数.
+    private int primeCount(int n) {
+        int[] isPrime = new int[n];
+        Arrays.fill(isPrime, 1);
+        int res = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime[i] == 1) {
+                ++res;
+            }
+            if ((long) i * i >= n) {
+                continue;
+            }
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = 0;
+            }
+        }
+        return res;
+    }
+
+}
