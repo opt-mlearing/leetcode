d9aaf03b47f5e748c76ad7eea8ff27a702b89847
caogaoli <caogaoli@baidu.com>
2022-04-09 02:20:40 +0800

    stack

---
+++ src/main/java/leetcode/SolutionOffice2_038.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 剑指 Offer II 038. 每日温度
+ * https://leetcode-cn.com/problems/iIQa4I/
+ */
+public class SolutionOffice2_038 {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int size = temperatures.length;
+        int[] res = new int[size];
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = 0; i < size; ++i) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                res[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+
+}
