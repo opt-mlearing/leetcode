159a7ee614b6f58edc10de44c6f1f921a5848ca3
caogaoli <caogaoli@baidu.com>
2022-04-13 23:53:48 +0800

    剑指 Offer 25. 合并两个排序的链表

---
+++ src/main/java/leetcode/offer/SolutionOffer_25.java
@@ -0,0 +1,34 @@
+package leetcode.offer;
+
+/**
+ * 剑指 Offer 25. 合并两个排序的链表
+ * https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
+ */
+public class SolutionOffer_25 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
