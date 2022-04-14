4e9e2bf7c755a7abc019890c0d942c1151327d26
caogaoli <caogaoli@baidu.com>
2022-04-14 21:02:44 +0800

    12. 整数转罗马数字

---
+++ src/main/java/leetcode/Solution12.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * 12. 整数转罗马数字
+ * https://leetcode-cn.com/problems/integer-to-roman/
+ */
+public class Solution12 {
+
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] str = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public String intToRoman(int num) {
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < values.length; ++i) {
+            while (num >= values[i]) {
+                num -= values[i];
+                buffer.append(str[i]);
+            }
+            if (num == 0) {
+                return buffer.toString();
+            }
+        }
+        return buffer.toString();
+    }
+
+}
