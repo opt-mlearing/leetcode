d3e2ca81a3961f12d4b45d908c9454c2e6fd91e1
caogaoli <caogaoli@baidu.com>
2022-04-20 00:26:39 +0800

    面试题 04.08. 首个共同祖先

---
+++ src/main/java/leetcode/typical/Solution04_08.java
@@ -0,0 +1,34 @@
+package leetcode.typical;
+
+/**
+ * 面试题 04.08. 首个共同祖先
+ * https://leetcode-cn.com/problems/first-common-ancestor-lcci/
+ */
+public class Solution04_08 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        }
+        // left== null && right== null.
+        return root;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
