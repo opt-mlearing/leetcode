57bad00a59267a59c85587c879910cdd8b4d0c11
caogaoli <caogaoli@baidu.com>
2022-04-19 23:27:31 +0800

    1123. 最深叶节点的最近公共祖先

--- src/main/java/leetcode/base/Solution1123.java
+++ src/main/java/leetcode/base/Solution1123.java
@@ -7,25 +7,53 @@ package leetcode.base;
 public class Solution1123 {
 
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        if (root == null) {
-            return root;
-        }
-        int left = findDepth(root.left);
-        int right = findDepth(root.right);
-        if (left == right) {
-            return root;
-        }
-        if (left > right) {
-            return lcaDeepestLeaves(root.left);
-        }
-        return lcaDeepestLeaves(root.right);
+        return dfs(root).node;
     }
 
-    private int findDepth(TreeNode root) {
+    private Record dfs(TreeNode root) {
         if (root == null) {
+            return new Record(0, null);
+        }
+        Record left = dfs(root.left);
+        Record right = dfs(root.right);
+        if (left.depth > right.depth) {
+            return new Record(left.depth + 1, left.node);
+        } else if (left.depth < right.depth) {
+            return new Record(right.depth + 1, right.node);
+        }
+        return new Record(left.depth + 1, root);
+    }
+
+    private static class Record {
+        private int depth;
+        private TreeNode node;
+
+        public Record(int depth, TreeNode node) {
+            this.depth = depth;
+            this.node = node;
+        }
+    }
+
+    // 多次循环迭代，反复求高度.
+    public TreeNode lcaDeepestLeaves_1(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        if (left == right) {
+            return root;
+        } else if (left < right) {
+            return lcaDeepestLeaves(root.right);
+        }
+        return lcaDeepestLeaves(root.left);
+    }
+
+    private int maxDepth(TreeNode node) {
+        if (node == null) {
             return 0;
         }
-        return 1 + Math.max(findDepth(root.left), findDepth(root.right));
+        return 1 + Math.max(maxDepth(node.left), maxDepth(node.right));
     }
 
     private static class TreeNode {
