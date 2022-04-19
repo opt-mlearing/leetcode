dfce31e6cc490b50f8e90c0292f893f4c440e0cc
caogaoli <caogaoli@baidu.com>
2022-04-20 00:17:59 +0800

    剑指 Offer 28. 对称的二叉树

---
+++ src/main/java/leetcode/offer/SolutionOffer28.java
@@ -0,0 +1,33 @@
+package leetcode.offer;
+
+/**
+ * 剑指 Offer 28. 对称的二叉树
+ * https://leetcode-cn.com/problems/dui-cheng-de-er-cha-shu-lcof/
+ */
+public class SolutionOffer28 {
+
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root, root);
+    }
+
+    private boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null ^ right == null) {
+            return false;
+        }
+        return left.val == right.val && dfs(left.left, right.right) && dfs(left.right, right.left);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
