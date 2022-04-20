7792b940bfc6bdc791d4eb448e4516bc11263a5f
caogaoli <caogaoli@baidu.com>
2022-04-21 01:14:25 +0800

    剑指 Offer 55 - II. 平衡二叉树

---
+++ src/main/java/leetcode/offer/SolutionOffer55_II.java
@@ -0,0 +1,39 @@
+package leetcode.offer;
+
+/**
+ * 剑指 Offer 55 - II. 平衡二叉树
+ * https://leetcode-cn.com/problems/ping-heng-er-cha-shu-lcof/
+ */
+public class SolutionOffer55_II {
+
+    private boolean res = true;
+
+    public boolean isBalanced(TreeNode root) {
+        res = true;
+        dfs(root);
+        return res;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        if (Math.abs(left - right) > 1) {
+            res = false;
+        }
+        return 1 + Math.max(left, right);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
