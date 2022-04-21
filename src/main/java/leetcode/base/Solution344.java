5d3e77d04161cc5b2687a194631da04eb0ecf0f2
caogaoli <caogaoli@baidu.com>
2022-04-21 16:52:23 +0800

    344. 反转字符串

--- src/main/java/leetcode/base/Solution344.java
+++ src/main/java/leetcode/base/Solution344.java
@@ -1,7 +1,7 @@
 package leetcode.base;
 
 /**
- * 反转字符串.
+ * 344. 反转字符串
  * https://leetcode-cn.com/problems/reverse-string/submissions/
  */
 public class Solution344 {
@@ -9,7 +9,7 @@ public class Solution344 {
     public void reverseString(char[] s) {
         int left = 0;
         int right = s.length - 1;
-        while (left <= right) {
+        while (left < right) {
             char tmp = s[left];
             s[left++] = s[right];
             s[right--] = tmp;
