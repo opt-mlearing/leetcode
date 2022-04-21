a8942ecc57a49dbc99770cacfc50022d91087924
caogaoli <caogaoli@baidu.com>
2022-04-21 16:01:46 +0800

    151. 颠倒字符串中的单词

--- src/main/java/leetcode/base/Solution151.java
+++ src/main/java/leetcode/base/Solution151.java
@@ -1,47 +1,75 @@
 package leetcode.base;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-
 /**
- * 颠倒字符串中的单词
+ * 151. 颠倒字符串中的单词
  * https://leetcode-cn.com/problems/reverse-words-in-a-string/
  */
 public class Solution151 {
 
     public String reverseWords(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-        while (left <= right) {
-            if (s.charAt(left) != ' ' && s.charAt(right) != ' ') {
-                break;
-            } else if (s.charAt(left) == ' ') {
-                left++;
-            } else if (s.charAt(right) == ' ') {
-                right--;
+        s = removeExtralSpaces(s);
+        s = reverse(s, 0, s.length() - 1);
+        int left = -1;
+        int right = -1;
+        boolean entryWord = false;
+        for (int i = 0; i < s.length(); ++i) {
+            if (!entryWord || (s.charAt(i) != ' ' && s.charAt(i - 1) == ' ')) {
+                left = i;
+                entryWord = true;
+            }
+            if (entryWord && s.charAt(i) == ' ' && s.charAt(i - 1) != ' ') {
+                right = i - 1;
+                entryWord = false;
+                s = reverse(s, left, right);
+            }
+            // 特殊处理组后一个单词
+            if (entryWord && s.charAt(i) != ' ' && i == s.length() - 1) {
+                right = i;
+                entryWord = false;
+                s = reverse(s, left, right);
             }
         }
-        Deque<String> deque = new ArrayDeque<String>();
-        StringBuffer buffer = new StringBuffer();
-        while (left <= right) {
-            if (s.charAt(left) != ' ') {
-                buffer.append(s.charAt(left));
+        return s;
+
+    }
+
+    // 反转整个字符串
+    private String reverse(String s, int left, int right) {
+        char[] chars = s.toCharArray();
+        while (left < right) {
+            char tmp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = tmp;
+            left++;
+            right--;
+        }
+        return new String(chars);
+    }
+
+    // 去除多余空格
+    private String removeExtralSpaces(String s) {
+        char[] chars = s.toCharArray();
+        int len = chars.length;
+        int slow = 0;
+        int fast = 0;
+        // 删除字符串前面的空格
+        while (len > 0 && fast < len && chars[fast] == ' ') {
+            fast++;
+        }
+        // 删除字符串间的空格
+        for (; fast < len; ++fast) {
+            if (fast > 0 && chars[fast] == chars[fast - 1] && chars[fast] == ' ') {
+                continue;
             } else {
-                if (buffer.length() > 0) {
-                    deque.push(buffer.toString());
-                    buffer.setLength(0);
-                }
+                chars[slow++] = chars[fast];
             }
-            ++left;
         }
-        if (buffer.length() > 0) {
-            deque.push(buffer.toString());
+        // 删除字符串后的空格
+        if (slow > 0 && chars[slow - 1] == ' ') {
+            return new String(chars, 0, slow - 1);
+        } else {
+            return new String(chars, 0, slow);
         }
-        String result = new String();
-        while (!deque.isEmpty()) {
-            result += deque.pop() + " ";
-        }
-        return result.substring(0, result.length() - 1);
     }
 
 }
