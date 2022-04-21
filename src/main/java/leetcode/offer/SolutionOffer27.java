70716815875985e7fe2acca335789e93eba63548
caogaoli <caogaoli@baidu.com>
2022-04-21 18:53:40 +0800

    剑指 Offer 27. 二叉树的镜像

---
+++ src/main/java/leetcode/offer/SolutionOffer27.java
@@ -0,0 +1,30 @@
+package leetcode.offer;
+
+/**
+ * 剑指 Offer 27. 二叉树的镜像
+ * https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
+ */
+public class SolutionOffer27 {
+
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
