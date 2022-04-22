e196980b0df2abee0b43ac60e39c36c86c610425
caogaoli <caogaoli@baidu.com>
2022-04-22 22:41:12 +0800

    40. 组合总和 II

--- src/main/java/leetcode/base/Solution40.java
+++ src/main/java/leetcode/base/Solution40.java
@@ -3,8 +3,10 @@ package leetcode.base;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 40. 组合总和 II
@@ -14,7 +16,45 @@ public class Solution40 {
 
     private List<List<Integer>> res;
 
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    // 方式1.
+    public List<List<Integer>> combinationSum2_1(int[] candidates, int target) {
+        res = new ArrayList<>();
+        Arrays.sort(candidates);
+        List<int[]> frequency = new ArrayList<int[]>();
+        for (int i = 0; i < candidates.length; ++i) {
+            if (frequency.isEmpty() || candidates[i] != candidates[i - 1]) {
+                frequency.add(new int[]{candidates[i], 1});
+            } else {
+                frequency.get(frequency.size() - 1)[1]++;
+            }
+        }
+        backTrack(frequency, target, 0, 0, new LinkedList<Integer>());
+        return res;
+    }
+
+    private void backTrack(List<int[]> frequency, int target, int sum, int startIndex, Deque<Integer> path) {
+        if (sum == target) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        if (startIndex == frequency.size() || sum + frequency.get(startIndex)[0] > target) {
+            return;
+        }
+        // 直接跳过当前位置
+        backTrack(frequency, target, sum, startIndex + 1, path);
+        int count = Math.min(frequency.get(startIndex)[1], (target - sum) / frequency.get(startIndex)[0]);
+        int tmp = frequency.get(startIndex)[0];
+        for (int i = 1; i <= count; ++i) {
+            path.offer(tmp);
+            backTrack(frequency, target, sum + tmp * i, startIndex + 1, path);
+        }
+        for (int i = 1; i <= count; ++i) {
+            path.pollLast();
+        }
+    }
+
+    // candidates中存在多个重复元素，处理方式1
+    public List<List<Integer>> combinationSum2_2(int[] candidates, int target) {
         Arrays.sort(candidates);
         res = new ArrayList<>();
         backTracking(candidates, target, 0, new LinkedList<Integer>(), 0);
