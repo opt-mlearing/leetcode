016fa3126fccb08444a3db8b6f290590d74a9199
caogaoli <caogaoli@baidu.com>
2022-04-23 11:02:26 +0800

    509. 斐波那契数

--- src/main/java/leetcode/base/Solution509.java
+++ src/main/java/leetcode/base/Solution509.java
@@ -1,17 +1,44 @@
 package leetcode.base;
 
+/**
+ * 509. 斐波那契数
+ * https://leetcode-cn.com/problems/fibonacci-number/
+ */
 public class Solution509 {
 
-    // 斐波那契数列指的是这样一个数列：1，1，2，3，5，8，13，21，34，55，89...
-    // 特性：这个数列从第3项开始，每一项都等于前两项之和。
-    public int fib(int n) {
-        int[] dp = new int[n + 1 > 2 ? n + 1 : 2];
+    // 递归，11ms/37.9MB
+    public int fib_recursion(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib_recursion(n - 1) + fib_recursion(n - 2);
+    }
+
+    // 动态规划，0ms/ 38.3MB
+    public int fib_dp1(int n) {
+        int[] dp = new int[Math.max(n, 1) + 1];
         dp[0] = 0;
         dp[1] = 1;
-        for (int i = 2; i < n + 1; ++i) {
+        for (int i = 2; i <= n; ++i) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
+    }
+
+    // 动态规划，0ms/ 37.7MB
+    public int fib_dp2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            int sum = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = sum;
+        }
+        return dp[1];
     }
 
 }
