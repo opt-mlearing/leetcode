d1c0c3755856cd73f3a8d151d565b3705991af4c
caogaoli <caogaoli@baidu.com>
2022-04-23 11:29:19 +0800

    70. 爬楼梯

--- src/main/java/leetcode/base/Solution70.java
+++ src/main/java/leetcode/base/Solution70.java
@@ -1,16 +1,19 @@
 package leetcode.base;
 
-import java.util.Arrays;
-
+/**
+ * 70. 爬楼梯
+ * https://leetcode-cn.com/problems/climbing-stairs/
+ */
 public class Solution70 {
 
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1 < 3 ? 3 : n + 1];
-        Arrays.fill(dp, 0);
-        dp[0] = 0;
+        // dp[i]= dp[i- 1]+ dp[i- 2]
+        // dp[i] 表示到达第i阶的方案数为dp[i]个.
+        int[] dp = new int[n + 1 >= 3 ? n + 1 : 3];
+        // dp[0] 看起来并无意义，可以不进行初始化.
         dp[1] = 1;
         dp[2] = 2;
-        for (int i = 3; i < n + 1; ++i) {
+        for (int i = 3; i <= n; ++i) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
