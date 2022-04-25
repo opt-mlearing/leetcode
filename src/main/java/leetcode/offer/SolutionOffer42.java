dfb3d09b825b0c3ab93b94083292a0e2e768d2ed
caogaoli <caogaoli@baidu.com>
2022-04-25 13:56:36 +0800

    剑指 Offer 42. 连续子数组的最大和

---
+++ src/main/java/leetcode/offer/SolutionOffer42.java
@@ -0,0 +1,36 @@
+package leetcode.offer;
+
+/**
+ * 剑指 Offer 42. 连续子数组的最大和
+ * https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+ */
+public class SolutionOffer42 {
+
+    public int maxSubArray_1(int[] nums) {
+        // dp[i] 表示以第i结尾子串最大.
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            if (dp[i - 1] >= 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    // 优化：dp[i]的状态转移只与dp[i- 1]相关.
+    public int maxSubArray(int[] nums) {
+        int pre = nums[0];
+        int res = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            pre = Math.max(nums[i], pre + nums[i]);
+            res = Math.max(res, pre);
+        }
+        return res;
+    }
+
+}
