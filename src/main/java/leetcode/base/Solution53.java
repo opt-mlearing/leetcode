cf3c40772f74cffc77f2de2479c07aacbb89ba6c
caogaoli <caogaoli@baidu.com>
2022-04-25 11:19:10 +0800

    53. 最大子数组和

--- src/main/java/leetcode/base/Solution53.java
+++ src/main/java/leetcode/base/Solution53.java
@@ -1,16 +1,55 @@
 package leetcode.base;
 
+/**
+ * 53. 最大子数组和
+ * https://leetcode-cn.com/problems/maximum-subarray/submissions/
+ */
 public class Solution53 {
 
-    public int maxSubArray(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
+    // dp[i], 以第i个数结尾的连续子串和最大.
+    public int maxSubArray_1(int[] nums) {
+        // dp[i- 1]>= 0, dp[i]= dp[i- 1]+ num[i]
+        // dp[i- 1]< 0, dp[i]= dp[i];
+        int size = nums.length;
+        int[] dp = new int[size];
+        dp[0] = nums[0];
+        for (int i = 1; i < size; ++i) {
+            if (dp[i - 1] >= 0) {
+                dp[i] = dp[i - 1] + nums[i];
+            } else {
+                dp[i] = nums[i];
+            }
         }
+        int res = dp[0];
+        for (int i = 1; i < size; ++i) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    // 优化，如上因为dp[i]只与dp[i- 1]相关.
+    public int maxSubArray_2(int[] nums) {
+        int preMax = nums[0];
         int globalMax = nums[0];
-        int pre = 0;
-        for (int i = 0; i < nums.length; ++i) {
-            pre = Math.max(pre + nums[i], nums[i]);
-            globalMax = Math.max(globalMax, pre);
+        for (int i = 1; i < nums.length; ++i) {
+            if (preMax >= 0) {
+                preMax = preMax + nums[i];
+            } else {
+                preMax = nums[i];
+            }
+            globalMax = Math.max(preMax, globalMax);
+        }
+        return globalMax;
+    }
+
+    // 再优化.
+    public int maxSubArray(int[] nums) {
+        // 注意，这里最小不能赋值Integer.MIN_VALUE,Integer.MIN_VALUE+负数就越界了.
+        int preMax = nums[0];
+        int globalMax = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            preMax = Math.max(nums[i], preMax + nums[i]);
+            globalMax = Math.max(globalMax, preMax);
         }
         return globalMax;
     }
