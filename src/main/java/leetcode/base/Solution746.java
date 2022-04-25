63a08bc31911e90cf6fbf4cb7ea519f392f1da09
caogaoli <caogaoli@baidu.com>
2022-04-25 16:48:28 +0800

    746. 使用最小花费爬楼梯

--- src/main/java/leetcode/base/Solution746.java
+++ src/main/java/leetcode/base/Solution746.java
@@ -1,7 +1,7 @@
 package leetcode.base;
 
 /**
- * 使用最小花费爬楼梯
+ * 746. 使用最小花费爬楼梯
  * https://leetcode-cn.com/problems/min-cost-climbing-stairs/
  */
 public class Solution746 {
@@ -9,13 +9,15 @@ public class Solution746 {
     public int minCostClimbingStairs(int[] cost) {
         // dp[i]= min(dp[i- 1]+ cost[n- 1], dp[n- 2]+ cost[n- 2])
         // dp[i] 表示到达 i的最小费用
-        int[] dp = new int[cost.length + 1];
+        int size = cost.length;
+        int[] dp = new int[size + 1];
+        // 第0和第1台阶花费为0.
         dp[0] = 0;
         dp[1] = 0;
-        for (int i = 2; i <= cost.length; ++i) {
+        for (int i = 2; i <= size; ++i) {
             dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
         }
-        return dp[cost.length];
+        return dp[size];
     }
 
 }
