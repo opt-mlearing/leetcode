f4ee0a9b0cb75a295556cf8c73dcf1586308afa0
caogaoli <caogaoli@baidu.com>
2022-04-25 10:07:45 +0800

    剑指 Offer 10- I. 斐波那契数列

---
+++ src/main/java/leetcode/offer/SolutionOffer1_10.java
@@ -0,0 +1,20 @@
+package leetcode.offer;
+
+/**
+ * 剑指 Offer 10- I. 斐波那契数列
+ * https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+ */
+public class SolutionOffer1_10 {
+
+    public int fib(int n) {
+        int size = n + 1 >= 2 ? n + 1 : 2;
+        int[] dp = new int[size];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        return dp[n];
+    }
+
+}
