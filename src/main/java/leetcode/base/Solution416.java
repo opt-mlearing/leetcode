b6074b46c864e5e8b850b1970bf189704495629a
caogaoli <caogaoli@baidu.com>
2022-04-28 11:24:12 +0800

    416. 分割等和子集

--- src/main/java/leetcode/base/Solution416.java
+++ src/main/java/leetcode/base/Solution416.java
@@ -7,7 +7,8 @@ package leetcode.base;
 public class Solution416 {
 
     // 正好分割成2个子集合
-    public boolean canPartition(int[] nums) {
+    // 这里寻找的是容量为target的背包最多能装多少货物.
+    public boolean canPartition_1(int[] nums) {
         if (nums == null || nums.length < 2) {
             return false;
         }
@@ -26,6 +27,29 @@ public class Solution416 {
             }
         }
         return dp[target] == target;
+    }
+
+    // 这里换一个思路，容量为target的背包是否存在.
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return false;
+        }
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        int target = sum / 2;
+        boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int j = target; j >= num; --j) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        return dp[target];
     }
 
 }
