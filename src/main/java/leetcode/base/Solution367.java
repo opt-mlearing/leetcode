bffc2b8aec65fc92777546ba55f26f8a64c255aa
caogaoli <caogaoli@baidu.com>
2022-05-04 01:01:32 +0800

    397. 整数替换

---
+++ src/main/java/leetcode/base/Solution367.java
@@ -0,0 +1,19 @@
+package leetcode.base;
+
+/**
+ * 397. 整数替换
+ * https://leetcode-cn.com/problems/integer-replacement/
+ */
+public class Solution367 {
+
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return integerReplacement(n / 2) + 1;
+        }
+        return Math.min(integerReplacement(n / 2), integerReplacement(n / 2 + 1)) + 2;
+    }
+
+}
