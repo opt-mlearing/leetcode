7c9bab9b776f3912fcad233d075812f956f4f31e
caogl <caogaoli058@163.com>
2022-05-14 23:46:50 +0800

    367. 有效的完全平方数

--- src/main/java/leetcode/base/Solution367.java
+++ src/main/java/leetcode/base/Solution367.java
@@ -1,19 +1,54 @@
 package leetcode.base;
 
 /**
- * 397. 整数替换
- * https://leetcode-cn.com/problems/integer-replacement/
+ * 367. 有效的完全平方数
+ * https://leetcode.cn/problems/valid-perfect-square/
  */
 public class Solution367 {
 
-    public int integerReplacement(int n) {
-        if (n == 1) {
-            return 0;
+    public boolean isPerfectSquare_1(int num) {
+        int left = 1;
+        int right = num;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= num) {
+                left = mid;
+            } else {
+                right = mid;
+            }
         }
-        if (n % 2 == 0) {
-            return integerReplacement(n / 2) + 1;
+        System.out.println("left: " + left + " right: " + right);
+        return left * left == num || right * right == num;
+    }
+
+    public boolean isPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+        while (left < right) {
+            int mid = left + (right - left + 1) / 2;
+            if ((long) mid * mid <= num) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
         }
-        return Math.min(integerReplacement(n / 2), integerReplacement(n / 2 + 1)) + 2;
+        System.out.println("left: " + left + " right: " + right);
+        return right * right == num;
+    }
+
+    public boolean isPerfectSquare_3(int num) {
+        int left = 1;
+        int right = num;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("left: " + left + " right: " + right);
+        return right * right == num;
     }
 
 }
