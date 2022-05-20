9382e8ca3873e6e592ad57c1480db7196b7f2ba3
caogaoli <caogaoli@baidu.com>
2022-05-20 11:04:42 +0800

    62. 不同路径

--- src/main/java/leetcode/base/Solution62.java
+++ src/main/java/leetcode/base/Solution62.java
@@ -1,12 +1,15 @@
 package leetcode.base;
 
+import java.util.Arrays;
+
 /**
- * 不同路径
+ * 62. 不同路径
  * https://leetcode-cn.com/problems/unique-paths/
  */
 public class Solution62 {
 
-    public int uniquePaths(int m, int n) {
+    // 二维dp
+    public int uniquePaths_1(int m, int n) {
         int[][] dp = new int[m + 1][n + 1];
         // dp[i][j]= dp[i- 1][j]+ dp[i][j- 1];
         // 注意边界.
@@ -22,6 +25,34 @@ public class Solution62 {
             }
         }
         return dp[m - 1][n - 1];
+    }
+
+    // 一维dp
+    public int uniquePaths_2(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+
+    // dfs 可能会超出时间限制.
+    public int uniquePaths_3(int m, int n) {
+        return dfs(0, 0, m, n);
+    }
+
+    private int dfs(int row, int column, int m, int n) {
+        if (row < 0 || row >= m || column < 0 || column >= n) {
+            return 0;
+        }
+        if (row == m - 1 && column == n - 1) {
+            return 1;
+        }
+        int count = dfs(row + 1, column, m, n) + dfs(row, column + 1, m, n);
+        return count;
     }
 
 }
