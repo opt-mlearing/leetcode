00702220dbb4d9253f77c0bfc6530eaa1ccbd0e6
caogaoli <caogaoli@baidu.com>
2022-05-27 16:24:53 +0800

    1302. 层数最深叶子节点的和

--- src/main/java/leetcode/base/Solution1302.java
+++ src/main/java/leetcode/base/Solution1302.java
@@ -9,28 +9,56 @@ import java.util.LinkedList;
  */
 public class Solution1302 {
 
+    private int res;
+
     public int deepestLeavesSum(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        Deque<TreeNode> deque = new LinkedList<TreeNode>();
-        deque.offer(root);
+        int maxDepth = maxDepth(root);
+        res = 0;
+        dfs(root, 1, maxDepth);
+        return res;
+    }
+
+    private int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    private void dfs(TreeNode root, int depth, int maxDepth) {
+        if (root == null) {
+            return;
+        }
+        if (depth == maxDepth) {
+            res += root.val;
+        }
+        dfs(root.left, depth + 1, maxDepth);
+        dfs(root.right, depth + 1, maxDepth);
+    }
+
+    // bfs
+    public int deepestLeavesSum_bfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        stack.offer(root);
         int res = 0;
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            int sum = 0;
+        while (!stack.isEmpty()) {
+            int size = stack.size();
+            res = 0;
             for (int i = 0; i < size; ++i) {
-                TreeNode node = deque.poll();
-                sum += node.val;
+                TreeNode node = stack.poll();
+                res += node.val;
                 if (node.left != null) {
-                    deque.offer(node.left);
+                    stack.offer(node.left);
                 }
                 if (node.right != null) {
-                    deque.offer(node.right);
+                    stack.offer(node.right);
                 }
-            }
-            if (deque.isEmpty()) {
-                res = sum;
             }
         }
         return res;
