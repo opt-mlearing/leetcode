2b714fcb309638b158b97c613d35e9d0028da26f
caogaoli <caogaoli@baidu.com>
2022-06-02 16:25:59 +0800

    373. 查找和最小的 K 对数字

--- src/main/java/leetcode/base/Solution373.java
+++ src/main/java/leetcode/base/Solution373.java
@@ -1,8 +1,45 @@
 package leetcode.base;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
 
+/**
+ * 373. 查找和最小的 K 对数字
+ * https://leetcode.cn/problems/find-k-pairs-with-smallest-sums/
+ */
 public class Solution373 {
+
+    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<int[]> priorityQueue =
+                new PriorityQueue<>(k, (o1, o2) -> nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]]);
+        int m = nums1.length;
+        int n = nums2.length;
+        // 其实只要保证[i, j],i== 0 && j== 0 一定在里面就ok.
+        for (int i = 0; i < Math.min(k, m); ++i) {
+            priorityQueue.offer(new int[]{i, 0});
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        // 选取出k个就结束.
+        while (k > 0 && !priorityQueue.isEmpty()) {
+            // 拿出最小的那个.
+            int[] item = priorityQueue.poll();
+            // index1+ 1的已经存在 priorityQueue中.
+            int index1 = item[0];
+            int index2 = item[1];
+            List<Integer> subList = new ArrayList<>();
+            subList.add(nums1[index1]);
+            subList.add(nums2[index2]);
+            res.add(subList);
+            if (index2 + 1 < n) {
+                priorityQueue.offer(new int[]{index1, index2 + 1});
+            }
+            k--;
+        }
+        return res;
+    }
 
     private Deque<int[]> stack;
 
