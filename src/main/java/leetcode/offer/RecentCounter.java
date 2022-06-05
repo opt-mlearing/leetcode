b318d6b4811a5ef039a18b041d19228b1bbea4f8
caogaoli <caogaoli@baidu.com>
2022-06-06 00:49:41 +0800

    剑指 Offer II 042. 最近请求次数

---
+++ src/main/java/leetcode/offer/RecentCounter.java
@@ -0,0 +1,32 @@
+package leetcode.offer;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 剑指 Offer II 042. 最近请求次数
+ * https://leetcode.cn/problems/H8086Q/submissions/
+ */
+public class RecentCounter {
+
+    private Deque<Integer> stack;
+
+    public RecentCounter() {
+        stack = new LinkedList<Integer>();
+    }
+
+    public int ping(int t) {
+        while (!stack.isEmpty() && t - stack.peek() > 3000) {
+            stack.poll();
+        }
+        stack.offer(t);
+        return stack.size();
+    }
+
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
