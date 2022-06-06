91af10840634525ec3a83950af909573145ad984
caogl <caogaoli058@163.com>
2022-06-06 23:45:34 +0800

    605. 种花问题

---
+++ src/main/java/leetcode/base/Solution605.java
@@ -0,0 +1,31 @@
+package leetcode.base;
+
+/**
+ * 605. 种花问题
+ * https://leetcode.cn/problems/can-place-flowers/
+ */
+public class Solution605 {
+
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int size = flowerbed.length;
+        int i = 0;
+        while (i < size) {
+            if (flowerbed[i] == 0) {
+                if (i == size - 1) {
+                    n--;
+                    break;
+                }
+                if (flowerbed[i + 1] == 0) {  // 00 种花
+                    n--;
+                    i += 2;
+                } else if (flowerbed[i + 1] == 1) {  // 010 不种花
+                    i+= 3;
+                }
+            } else if (flowerbed[i] == 1) {  // 10 不种花
+                i+= 2;
+            }
+        }
+        return n <= 0;
+    }
+
+}
