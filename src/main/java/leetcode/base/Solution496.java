1b2114db09765a659aa820c32a98a08c7fe0e351
caogl <caogaoli058@163.com>
2022-06-08 02:08:13 +0800

    496. 下一个更大元素 I

--- src/main/java/leetcode/base/Solution496.java
+++ src/main/java/leetcode/base/Solution496.java
@@ -7,12 +7,32 @@ import java.util.LinkedList;
 import java.util.Map;
 
 /**
- * 下一个更大元素 I
+ * 496. 下一个更大元素 I
  * https://leetcode-cn.com/problems/next-greater-element-i/
  */
 public class Solution496 {
 
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int m = nums1.length;
+        int n = nums2.length;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = n - 1; i >= 0; --i) {
+            // 栈中存最大的.
+            while (!stack.isEmpty() && (stack.peek() <= nums2[i])) {
+                stack.pop();
+            }
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
+            stack.push(nums2[i]);
+        }
+        int[] res = new int[m];
+        for (int i = 0; i < m; ++i) {
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+
+    public int[] nextGreaterElement_1(int[] nums1, int[] nums2) {
         // 构建hash映射.
         Map<Integer, Integer> mapping = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums2.length; ++i) {
