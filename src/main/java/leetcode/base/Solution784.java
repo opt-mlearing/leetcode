ed635fae615db619e4f550a670e3d0126d99a013
caogl <caogaoli058@163.com>
2022-06-15 11:49:29 +0800

    784. 字母大小写全排列

--- src/main/java/leetcode/base/Solution784.java
+++ src/main/java/leetcode/base/Solution784.java
@@ -4,14 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 字母大小写全排列
+ * 784. 字母大小写全排列
  * https://leetcode-cn.com/problems/letter-case-permutation/
- */
+ *
+ * @author: caogl
+ * @date: 2022/6/15, 11:48
+ **/
 public class Solution784 {
 
     private List<String> res = null;
 
     public List<String> letterCasePermutation(String s) {
+        res = new ArrayList<String>();
+        char[] chars = s.toCharArray();
+        backTracking(chars, 0);
+        return res;
+    }
+
+    private void backTracking(char[] chars, int index) {
+        if (index == chars.length) {
+            res.add(new String(chars));
+            return;
+        }
+        backTracking(chars, index + 1);
+        Character tmp = chars[index];
+        if (Character.isLetter(tmp)) {
+            if (Character.isLowerCase(tmp)) {
+                chars[index] = Character.toUpperCase(tmp);
+                backTracking(chars, index + 1);
+                chars[index] = Character.toLowerCase(tmp);
+            } else {
+                chars[index] = Character.toLowerCase(tmp);
+                backTracking(chars, index + 1);
+                chars[index] = Character.toUpperCase(tmp);
+            }
+        }
+    }
+
+    public List<String> letterCasePermutation_dfs(String s) {
         res = new ArrayList<String>();
         char[] chars = s.toCharArray();
         dfs(chars, 0);
