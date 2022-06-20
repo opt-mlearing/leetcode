465da31a8e2cbaced0645a15da9d3b75b56bf9ec
caogl <caogaoli058@163.com>
2022-06-20 23:00:20 +0800

    70. 爬楼梯

--- src/main/java/leetcode/base/Solution70.java
+++ src/main/java/leetcode/base/Solution70.java
@@ -6,7 +6,26 @@ package leetcode.base;
  */
 public class Solution70 {
 
-    public int climbStairs(int n) {
+    private Integer[] memo;
+
+    public int climbStairs_memo(int n) {
+        memo = new Integer[n + 1 >= 3 ? n + 1 : 3];
+        memo[0] = 0;
+        memo[1] = 1;
+        memo[2] = 2;
+        return dfs(n);
+    }
+
+    private int dfs(int n) {
+        if (memo[n] != null) {
+            return memo[n];
+        }
+        int count = dfs(n - 1) + dfs(n - 2);
+        memo[n] = count;
+        return count;
+    }
+
+    public int climbStairs_dp(int n) {
         // dp[i]= dp[i- 1]+ dp[i- 2]
         // dp[i] 表示到达第i阶的方案数为dp[i]个.
         int[] dp = new int[n + 1 >= 3 ? n + 1 : 3];
