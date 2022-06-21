e9ef8d6a02dce98fe20dad15f63eebcab787fd20
caogl <caogaoli058@163.com>
2022-06-21 21:44:34 +0800

    1140. 石子游戏 II

--- src/main/java/leetcode/base/Solution1140.java
+++ src/main/java/leetcode/base/Solution1140.java
@@ -1,82 +1,82 @@
 package leetcode.base;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * 1140. 石子游戏 II
  * https://leetcode.cn/problems/stone-game-ii/
- */
+ *
+ * @author: caogl
+ * @date: 2022/6/21, 21:43
+ **/
 public class Solution1140 {
 
-    private Map<Integer, Map<Integer, Integer>> record;
-    private int[] preSum;
-
-    // dfs && 记忆化搜索.
-    public int stoneGameII_dfs(int[] piles) {
+    // 递归+记忆化搜索. 3ms/40.9mb.
+    public int stoneGameII_recursion(int[] piles) {
         int size = piles.length;
-        preSum = new int[size + 1];
-        record = new HashMap<Integer, Map<Integer, Integer>>();
-        for (int i = 0; i < size; ++i) {
-            record.put(i, new HashMap<Integer, Integer>());
-            preSum[i + 1] = preSum[i] + piles[i];
+        int[] postSum = new int[size + 1];
+        for (int i = size - 1; i >= 0; --i) {
+            postSum[i] = postSum[i + 1] + piles[i];
         }
-        return dfs(piles, size, 0, 1);
+        Integer[][] memo = new Integer[size][2 * size];
+        return dfs(piles, 0, 1, postSum, memo);
     }
 
-    private int dfs(int[] piles, int size, int start, int m) {
-        if (start >= size) {
+    private int dfs(int[] piles, int start, int m, int[] postSum, Integer[][] memo) {
+        if (start >= piles.length) {
             return 0;
         }
-        if (record.get(start).getOrDefault(m, -1) != -1) {
-            return record.get(start).get(m);
+        if (start + 2 * m >= piles.length) {
+            return postSum[start];
         }
-        int maxStone = 0;
+        if (memo[start][m] != null) {
+            return memo[start][m];
+        }
+        // value是对手得分，需要限制对手等分最少.
+        int value = Integer.MAX_VALUE;
         for (int x = 1; x <= 2 * m; ++x) {
-            int nextM = Math.max(m, x);
-            int nextMaxStore = dfs(piles, size, start + x, nextM);
-            maxStone = Math.max(maxStone, preSum[size] - preSum[start] - nextMaxStore);
+            int nextm = Math.max(x, m);
+            value = Math.min(value, dfs(piles, start + x, nextm, postSum, memo));
         }
-        record.get(start).put(m, maxStone);
-        return maxStone;
+        value = postSum[start] - value;
+        memo[start][m] = value;
+        return value;
     }
 
-    // dp
-    public int stoneGameII_dp1(int[] piles) {
+    // 动归，其中dp[i][m]表示在piles[i: size][m] 最多取m个的时候最大.
+    public int stoneGameII_dp_1(int[] piles) {
         int size = piles.length;
-        //dp[i][j] 表示从 piles[i: size- 1]取j个先手最大.
         int[][] dp = new int[size][size + 1];
         int sum = 0;
         for (int i = size - 1; i >= 0; --i) {
             sum += piles[i];
-            // 特殊值，当i== 1的时候，m最小取1.
+            // i== 0 时 m= 1;
             for (int m = 1; m <= i || m == 1; ++m) {
                 if (i + 2 * m >= size) {
                     dp[i][m] = sum;
-                } else {
-                    for (int x = 1; x <= 2 * m; ++x) {
-                        dp[i][m] = Math.max(dp[i][m], sum - dp[i + x][Math.max(m, x)]);
-                    }
+                    continue;
+                }
+                for (int x = 1; x <= 2 * m; ++x) {
+                    dp[i][m] = Math.max(dp[i][m], sum - dp[i + x][Math.max(x, m)]);
                 }
             }
         }
         return dp[0][1];
     }
 
-    public int stoneGameII_dp2(int[] piles) {
+    // 动归，其中dp[i][m]表示在piles[i: size][m] 最多取m个的时候最大.
+    public int stoneGameII(int[] piles) {
         int size = piles.length;
         int[][] dp = new int[size][size + 1];
         int sum = 0;
-        for (int i = size; i >= 1; --i) {
-            sum += piles[i - 1];
-            // i== 0 时 m= 1, 改进下i的最小值.
-            for (int m = 1; m <= i; ++m) {
-                if (i - 1 + 2 * m >= size) {
-                    dp[i - 1][m] = sum;
-                } else {
-                    for (int x = 1; x <= 2 * m; ++x) {
-                        dp[i - 1][m] = Math.max(dp[i - 1][m], sum - dp[i - 1 + x][Math.max(x, m)]);
-                    }
+        for (int i = size - 1; i >= 0; --i) {
+            sum += piles[i];
+            // i== 0 时 m= 1;
+            for (int m = 1; m <= size; ++m) {
+                if (i + 2 * m >= size) {
+                    dp[i][m] = sum;
+                    continue;
+                }
+                for (int x = 1; x <= 2 * m; ++x) {
+                    dp[i][m] = Math.max(dp[i][m], sum - dp[i + x][Math.max(x, m)]);
                 }
             }
         }
