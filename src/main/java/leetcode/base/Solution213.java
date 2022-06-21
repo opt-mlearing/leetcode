6266552004e1cd7546b880bd4d2cf97338506ef6
caogl <caogaoli058@163.com>
2022-06-22 01:19:30 +0800

    213. 打家劫舍 II

--- src/main/java/leetcode/base/Solution213.java
+++ src/main/java/leetcode/base/Solution213.java
@@ -1,32 +1,83 @@
 package leetcode.base;
 
 /**
- * 打家劫舍 II
- * https://leetcode-cn.com/problems/house-robber-ii/
- */
+ * 213. 打家劫舍 II
+ * https://leetcode.cn/problems/house-robber-ii/
+ *
+ * @author: caogl
+ * @date: 2022/6/22, 1:18
+ **/
 public class Solution213 {
 
+    // dp
     public int rob(int[] nums) {
-        if (nums.length == 1) {
+        int size = nums.length;
+        if (size == 1) {
             return nums[0];
-        } else if (nums.length == 2) {
+        } else if (size == 2) {
             return Math.max(nums[0], nums[1]);
         } else {
-            // 从0出发的，搜索到nums.length- 2的位置（包含）
-            // 出1出发的，搜索到nums.length- 1的位置（包含）
-            return Math.max(doRob(nums, 0, nums.length - 2), doRob(nums, 1, nums.length - 1));
+            return Math.max(rob(nums, 0, size - 2), rob(nums, 1, size - 1));
         }
     }
 
-    private int doRob(int[] nums, int start, int end) {
-        int first = nums[start];
-        int second = Math.max(nums[start], nums[start + 1]);
-        for (int i = start + 2; i <= end; ++i) {
+    private int rob(int[] nums, int left, int right) {
+        int[] dp = new int[right + 1];
+        dp[left] = nums[left];
+        dp[left + 1] = Math.max(nums[left], nums[left + 1]);
+        for (int i = left + 2; i <= right; ++i) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[right];
+    }
+
+    // dp,压缩空间.
+    public int rob_dp1(int[] nums) {
+        int size = nums.length;
+        if (size == 1) {
+            return nums[0];
+        } else if (size == 2) {
+            return Math.max(nums[0], nums[1]);
+        } else {
+            return Math.max(robDp(nums, 0, size - 2), robDp(nums, 1, size - 1));
+        }
+    }
+
+    private int robDp(int[] nums, int left, int right) {
+        int first = nums[left];
+        int second = Math.max(nums[left], nums[left + 1]);
+        for (int i = left + 2; i <= right; ++i) {
             int tmp = second;
             second = Math.max(first + nums[i], second);
             first = tmp;
         }
         return second;
+    }
+
+    // 递归+ 记忆化搜索
+    public int rob_recursion(int[] nums) {
+        int size = nums.length;
+        if (size == 1) {
+            return nums[0];
+        } else if (size == 2) {
+            return Math.max(nums[0], nums[1]);
+        } else {
+            return Math.max(dfs(nums, 0, size - 2, new Integer[size]),
+                    dfs(nums, 1, size - 1, new Integer[size]));
+        }
+    }
+
+    public int dfs(int[] nums, int left, int index, Integer[] memo) {
+        if (index < left) {
+            return 0;
+        }
+        if (null != memo[index]) {
+            return memo[index];
+        }
+        int val = Math.max(dfs(nums, left, index - 1, memo),
+                dfs(nums, left, index - 2, memo) + nums[index]);
+        memo[index] = val;
+        return val;
     }
 
 }
