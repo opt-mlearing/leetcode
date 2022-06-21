188894df082a303624ed74437623246d3a7afb8d
caogl <caogaoli058@163.com>
2022-06-21 21:41:52 +0800

    877. 石子游戏

--- src/main/java/leetcode/base/Solution877.java
+++ src/main/java/leetcode/base/Solution877.java
@@ -3,40 +3,79 @@ package leetcode.base;
 /**
  * 877. 石子游戏
  * https://leetcode-cn.com/problems/stone-game/
- */
+ *
+ * @author: caogl
+ * @date: 2022/6/21, 21:41
+ **/
 public class Solution877 {
 
-    // 一维度动态规划.
+    // 递归+记忆化搜索.
     public boolean stoneGame(int[] piles) {
+        int size = piles.length;
+        Integer[][] memo = new Integer[size][size];
+        for (int i = 0; i < size; ++i) {
+            memo[i][i] = piles[i];
+        }
+        return dfs(memo, 0, size - 1, piles) >= 0;
+    }
+
+    private int dfs(Integer[][] memo, int left, int right, int[] piles) {
+        if (left > right) {
+            return 0;
+        }
+        if (memo[left][right] != null) {
+            return memo[left][right];
+        }
+        int value = Math.max(piles[left] - dfs(memo, left + 1, right, piles),
+                piles[right] - dfs(memo, left, right - 1, piles));
+        memo[left][right] = value;
+        return value;
+    }
+
+    // 一维dp,4ms/39mb.
+    public boolean stoneGame_1(int[] piles) {
         int size = piles.length;
         int[] dp = new int[size];
         for (int i = 0; i < size; ++i) {
             dp[i] = piles[i];
         }
-        for (int i = size - 2; i >= 0; --i) {
+        for (int i = size - 1; i >= 0; --i) {
             for (int j = i + 1; j < size; ++j) {
                 dp[j] = Math.max(piles[i] - dp[j], piles[j] - dp[j - 1]);
             }
         }
-        return dp[size - 1] > 0;
+        return dp[size - 1] >= 0;
     }
 
-    // 二维动态规划.
-    public boolean stoneGame_2(int[] piles) {
+    // 二维dp,7ms/42mb.
+    public boolean stoneGame_21(int[] piles) {
         int size = piles.length;
-        // dp[i][j]表示当剩下i到j时，当前玩家与前面玩家石子数相差最大.
         int[][] dp = new int[size][size];
         for (int i = 0; i < size; ++i) {
             dp[i][i] = piles[i];
         }
-        // 注意这里i<= j dp[i][j] 才有意义.
-        for (int i = size - 2; i >= 0; --i) {
-            for (int j = i + 1; j < size; ++j) {
+        for (int j = size - 1; j >= 0; --j) {
+            for (int i = 0; i < j; ++i) {
                 dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
             }
         }
-        // Alice先手, Bob后手，确定无平局.
-        return dp[0][size - 1] > 0;
+        return dp[0][size - 1] >= 0;
+    }
+
+    // 2维dp,6ms/42.5mb.
+    public boolean stoneGame_22(int[] piles) {
+        int size = piles.length;
+        int[][] dp = new int[size][size];
+        for (int i = 0; i < size; ++i) {
+            dp[i][i] = piles[i];
+        }
+        for (int len = 1; len < size; ++len) {
+            for (int i = 0; i < size - len; ++i) {
+                int j = i + len;
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][size - 1] >= 0;
     }
 
 }
