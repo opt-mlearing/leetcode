b55c4517b432435caa81a8420e79966059e23ec3
caogl <caogaoli058@163.com>
2022-06-25 23:40:27 +0800

    119. 杨辉三角 II

--- src/main/java/leetcode/base/Solution119.java
+++ src/main/java/leetcode/base/Solution119.java
@@ -4,30 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 杨辉三角II
+ * 119. 杨辉三角 II
+ * https://leetcode.cn/problems/pascals-triangle-ii/
  *
- * @link https://leetcode-cn.com/problems/pascals-triangle-ii/
- */
+ * @author: caogl
+ * @date: 2022/6/25, 23:39
+ **/
 public class Solution119 {
 
-    // 注意题目中已经明确rowIndex为非负
-    // 先生成杨辉三角，再返回最后一行.
+    // dp.
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < rowIndex + 1; ++i) {
+        int[][] dp = new int[rowIndex + 1][rowIndex + 1];
+        List<Integer> res = new ArrayList<Integer>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i <= rowIndex; ++i) {
+            for (int j = 1; j < i; ++j) {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            }
+        }
+        for (int i = 0; i <= rowIndex; ++i) {
+            res.add(dp[rowIndex][i]);
+        }
+        return res;
+    }
+
+    // 先生成杨辉三角，再返回最后一样就好.
+    public List<Integer> getRow_traverse(int rowIndex) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; ++i) {
             List<Integer> tmp = new ArrayList<Integer>();
             for (int j = 0; j <= i; ++j) {
-                // 产生第一行时，一定不会进入到else分支.
                 if (j == 0 || j == i) {
                     tmp.add(1);
                 } else {
-                    List<Integer> preRow = result.get(i - 1);
-                    tmp.add(preRow.get(j - 1) + preRow.get(j));
+                    List<Integer> row = res.get(i - 1);
+                    tmp.add(row.get(j - 1) + row.get(j));
                 }
             }
-            result.add(tmp);
+            res.add(tmp);
         }
-        return result.get(rowIndex);
+        return res.get(rowIndex);
     }
 
 }
