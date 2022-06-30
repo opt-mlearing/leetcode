41afef368399a6f02d90da8d8be47a3e4a5749aa
caogl <caogaoli058@163.com>
2022-06-30 22:04:48 +0800

    740. 删除并获得点数

--- src/main/java/leetcode/base/Solution740.java
+++ src/main/java/leetcode/base/Solution740.java
@@ -5,13 +5,59 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * 删除并获得点数
- * https://leetcode-cn.com/problems/delete-and-earn/
- */
+ * 740. 删除并获得点数
+ * https://leetcode.cn/problems/delete-and-earn/
+ *
+ * @author: caogl
+ * @date: 2022/6/30, 22:02
+ **/
 public class Solution740 {
 
-    // 现将连续数字，变成相邻，然后套用打家劫舍的例子.
+    // 1ms/ 40.8mb
     public int deleteAndEarn(int[] nums) {
+        int maxVal = nums[0];
+        int size = nums.length;
+        for (int i = 1; i < size; ++i) {
+            maxVal = Math.max(maxVal, nums[i]);
+        }
+        int[] arr = new int[maxVal + 1];
+        for (int i = 0; i < size; ++i) {
+            arr[nums[i]] += nums[i];
+        }
+        int first = arr[0];
+        int second = Math.max(arr[0], arr[1]);
+        for (int i = 2; i <= maxVal; ++i) {
+            int tmp = second;
+            first = Math.max(second, first + arr[i]);
+            second = first;
+            first = tmp;
+        }
+        return second;
+    }
+
+    // 2ms/ 41.2mb
+    public int deleteAndEarn_2(int[] nums) {
+        int maxVal = nums[0];
+        int size = nums.length;
+        for (int i = 1; i < size; ++i) {
+            maxVal = Math.max(maxVal, nums[i]);
+        }
+        int[] arr = new int[maxVal + 1];
+        for (int i = 0; i < size; ++i) {
+            arr[nums[i]] += nums[i];
+        }
+        int[] dp = new int[maxVal + 1];
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+        for (int i = 2; i <= maxVal; ++i) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + arr[i]);
+        }
+        return dp[maxVal];
+    }
+
+    // 8ms/ 41.5mb
+    // 现将连续数字，变成相邻，然后套用打家劫舍的例子.
+    public int deleteAndEarn_1(int[] nums) {
         Arrays.sort(nums);
         List<Integer> subList = new ArrayList<Integer>();
         subList.add(nums[0]);
