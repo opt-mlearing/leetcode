808ee8d52f3ab3f3fb77b372b11914c03c273b50
caogaoli <caogaoli@baidu.com>
2022-07-04 11:49:58 +0800

    [200~1034. 边界着色~

--- src/main/java/leetcode/base/Solution1034.java
+++ src/main/java/leetcode/base/Solution1034.java
@@ -1,9 +1,7 @@
 package leetcode.base;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 1034. 边界着色
@@ -16,21 +14,48 @@ public class Solution1034 {
 
     private static final int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     private List<int[]> list;
+    private Deque<int[]> stack;
 
-    public static void main(String[] args) {
-        Solution1034 solution = new Solution1034();
-        int[][] grid = {{1, 1}, {1, 2}};
-        int[][] arrays = solution.colorBorder(grid, 0, 0, 3);
-        for (int i = 0; i < arrays.length; ++i) {
-            System.out.println(Arrays.toString(arrays[i]));
+    // bfs
+    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        stack = new LinkedList<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] isVisit = new boolean[m][n];
+        stack.offer(new int[]{row, col});
+        isVisit[row][col] = true;
+        List<int[]> list = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            int[] top = stack.poll();
+            boolean isBorder = false;
+            for (int i = 0; i < direction.length; ++i) {
+                int nrow = top[0] + direction[i][0];
+                int ncol = top[1] + direction[i][1];
+                if (!(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && grid[top[0]][top[1]] == grid[nrow][ncol])) {
+                    isBorder = true;
+                } else if (!isVisit[nrow][ncol]) {
+                    isVisit[nrow][ncol] = true;
+                    stack.offer(new int[]{nrow, ncol});
+                }
+            }
+            if (isBorder) {
+                list.add(new int[]{top[0], top[1]});
+            }
         }
+        for (int i = 0; i < list.size(); ++i) {
+            int[] item = list.get(i);
+            grid[item[0]][item[1]] = color;
+        }
+        return grid;
     }
 
-    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+    // dfs
+    public int[][] colorBorder_dfs(int[][] grid, int row, int col, int color) {
         int m = grid.length;
         int n = grid[0].length;
         list = new ArrayList<int[]>();
         boolean[][] isVisit = new boolean[m][n];
+        isVisit[row][col] = true;
         dfs(grid, row, col, m, n, isVisit);
         for (int i = 0; i < list.size(); ++i) {
             int[] item = list.get(i);
