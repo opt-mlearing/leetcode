41ac76fbf9655e5d58b8aa9fdc57543c0af577c0
caogl <caogaoli058@163.com>
2022-07-08 01:29:16 +0800

    62. 不同路径

--- src/main/java/leetcode/base/Solution62.java
+++ src/main/java/leetcode/base/Solution62.java
@@ -4,13 +4,46 @@ import java.util.Arrays;
 
 /**
  * 62. 不同路径
- * https://leetcode-cn.com/problems/unique-paths/
- */
+ * https://leetcode.cn/problems/unique-paths/
+ *
+ * @author: caogl
+ * @date: 2022/7/8, 1:13
+ **/
 public class Solution62 {
 
+    private static final int[][] direction = {{0, -1}, {-1, 0}};
+
+    // 递归+ 记忆化搜索.
+    public int uniquePaths(int m, int n) {
+        Integer[][] memo = new Integer[m][n];
+        return dfs(m - 1, n - 1, memo, m, n);
+    }
+
+    private int dfs(int x, int y, Integer[][] memo, int m, int n) {
+        if (x < 0 || y < 0) {
+            return 0;
+        }
+        if (x == 0 || y == 0) {
+            return 1;
+        }
+        if (memo[x][y] != null) {
+            return memo[x][y];
+        }
+        int count = 0;
+        for (int i = 0; i < direction.length; ++i) {
+            int nx = x + direction[i][0];
+            int ny = y + direction[i][1];
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                count += dfs(nx, ny, memo, m, n);
+            }
+        }
+        memo[x][y] = count;
+        return count;
+    }
+
     // 二维dp
-    public int uniquePaths_1(int m, int n) {
-        int[][] dp = new int[m + 1][n + 1];
+    public int uniquePaths_2(int m, int n) {
+        int[][] dp = new int[m][n];
         // dp[i][j]= dp[i- 1][j]+ dp[i][j- 1];
         // 注意边界.
         for (int i = 0; i < n; ++i) {
@@ -28,7 +61,7 @@ public class Solution62 {
     }
 
     // 一维dp
-    public int uniquePaths_2(int m, int n) {
+    public int uniquePaths_1(int m, int n) {
         int[] dp = new int[n];
         Arrays.fill(dp, 1);
         for (int i = 1; i < m; ++i) {
@@ -37,22 +70,6 @@ public class Solution62 {
             }
         }
         return dp[n - 1];
-    }
-
-    // dfs 可能会超出时间限制.
-    public int uniquePaths_3(int m, int n) {
-        return dfs(0, 0, m, n);
-    }
-
-    private int dfs(int row, int column, int m, int n) {
-        if (row < 0 || row >= m || column < 0 || column >= n) {
-            return 0;
-        }
-        if (row == m - 1 && column == n - 1) {
-            return 1;
-        }
-        int count = dfs(row + 1, column, m, n) + dfs(row, column + 1, m, n);
-        return count;
     }
 
 }
