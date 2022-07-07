734879bb788bc82740f1c6e150c75681b0948e67
caogl <caogaoli058@163.com>
2022-07-08 02:14:50 +0800

    64. 最小路径和

--- src/main/java/leetcode/base/Solution64.java
+++ src/main/java/leetcode/base/Solution64.java
@@ -1,12 +1,46 @@
 package leetcode.base;
 
 /**
- * 最小路径和
- * https://leetcode-cn.com/problems/minimum-path-sum/
- */
+ * 64. 最小路径和
+ * https://leetcode.cn/problems/minimum-path-sum/
+ *
+ * @author: caogl
+ * @date: 2022/7/8, 2:14
+ **/
 public class Solution64 {
 
+    private static final int[][] direction = {{0, -1}, {-1, 0}};
+
+    // 递归+ 记忆化搜索.
     public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Integer[][] memo = new Integer[m][n];
+        return dfs(grid, m - 1, n - 1, m, n, memo);
+    }
+
+    private int dfs(int[][] grid, int x, int y, int m, int n, Integer[][] memo) {
+        if (x == 0 && y == 0) {
+            return grid[0][0];
+        }
+        if (memo[x][y] != null) {
+            return memo[x][y];
+        }
+        int count = Integer.MAX_VALUE;
+        for (int i = 0; i < direction.length; ++i) {
+            int nx = x + direction[i][0];
+            int ny = y + direction[i][1];
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+                count = Math.min(count, dfs(grid, nx, ny, m, n, memo));
+            }
+        }
+        count += grid[x][y];
+        memo[x][y] = count;
+        return count;
+    }
+
+    // 网格dp, 二维dp
+    public int minPathSum_dp(int[][] grid) {
         // dp[i][j]= grid[i][j]+ min(dp[i- 1][j], dp[i][j- 1]);
         int m = grid.length;
         int n = grid[0].length;
