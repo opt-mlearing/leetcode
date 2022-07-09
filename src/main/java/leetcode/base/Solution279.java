3015bda7d8f16c1bd5803c51591e5e9259a238a1
caogl <caogaoli058@163.com>
2022-07-10 01:32:26 +0800

    279. 完全平方数

--- src/main/java/leetcode/base/Solution279.java
+++ src/main/java/leetcode/base/Solution279.java
@@ -1,20 +1,38 @@
 package leetcode.base;
 
 /**
- * 完全平方数
- * https://leetcode-cn.com/problems/perfect-squares/
- */
+ * 279. 完全平方数
+ * https://leetcode.cn/problems/perfect-squares/
+ *
+ * @author: caogl
+ * @date: 2022/7/10, 1:31
+ **/
 public class Solution279 {
+
+    public int numSquares_1(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            int tmp = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; ++j) {
+                tmp = Math.min(tmp, dp[i - j * j]);
+            }
+            if (tmp != Integer.MAX_VALUE) {
+                dp[i] = tmp + 1;
+            }
+        }
+        return dp[n];
+    }
 
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         dp[0] = 0;
         for (int i = 1; i <= n; ++i) {
-            // 1的平方还是1.
-            dp[i] = dp[i - 1] + 1;
+            int tmp = Integer.MAX_VALUE;
             for (int j = 1; j * j <= i; ++j) {
-                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+                tmp = Math.min(tmp, dp[i - j * j] + 1);
             }
+            dp[i] = tmp;
         }
         return dp[n];
     }
