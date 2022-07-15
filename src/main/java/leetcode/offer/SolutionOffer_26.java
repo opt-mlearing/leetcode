e3bedc48ee7002de8da72c35664c7136ed6287a6
caogaoli <caogaoli@baidu.com>
2022-07-15 16:44:59 +0800

    剑指 Offer 26. 树的子结构

---
+++ src/main/java/leetcode/offer/SolutionOffer_26.java
@@ -0,0 +1,39 @@
+package leetcode.offer;
+
+/**
+ * 剑指 Offer 26. 树的子结构
+ * https://leetcode.cn/problems/shu-de-zi-jie-gou-lcof/
+ *
+ * @author caogaoli
+ * @date 2022/7/15 16:44
+ */
+public class SolutionOffer_26 {
+
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) {
+            return false;
+        }
+        return dfs(A, B) || isSubStructure(A.right, B) || isSubStructure(A.left, B);
+    }
+
+    private boolean dfs(TreeNode a, TreeNode b) {
+        if (b == null) {
+            return true;
+        }
+        if (a == null || a.val != b.val) {
+            return false;
+        }
+        return dfs(a.left, b.left) && dfs(a.right, b.right);
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
