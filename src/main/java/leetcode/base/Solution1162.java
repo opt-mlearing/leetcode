f45ecf3407e66933f0858f4041a77aff21d92831
caogl <caogaoli058@163.com>
2022-07-17 22:11:24 +0800

    1162. 地图分析

--- src/main/java/leetcode/base/Solution1162.java
+++ src/main/java/leetcode/base/Solution1162.java
@@ -14,6 +14,93 @@ public class Solution1162 {
 
     private static final int[][] direction = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
+    // 多源头bfs
+    // 14ms/ 42.7mb
+    public int maxDistance(int[][] grid) {
+        int n = grid.length;
+        int[][] dp = new int[n][n];
+        Deque<int[]> stack = new LinkedList<int[]>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i][j] = Integer.MAX_VALUE / 2;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    stack.offer(new int[]{i, j});
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            int[] item = stack.poll();
+            for (int i = 0; i < direction.length; ++i) {
+                int nx = item[0] + direction[i][0];
+                int ny = item[1] + direction[i][1];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && dp[nx][ny] > dp[item[0]][item[1]] + 1) {
+                    dp[nx][ny] = dp[item[0]][item[1]] + 1;
+                    stack.offer(new int[]{nx, ny});
+                }
+            }
+        }
+        int res = -1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                // 寻找水的地方.
+                if (grid[i][j] == 0) {
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE / 2 ? -1 : res;
+    }
+
+    // dp
+    // 7ms/ 42.7mb
+    public int maxDistance_dp(int[][] grid) {
+        int n = grid.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                // 最大值不能设置成Integer.MAX_VALUE, 需要/2, 若否则正好+1溢出.
+                dp[i][j] = (grid[i][j] == 1 ? 0 : Integer.MAX_VALUE / 2);
+            }
+        }
+        // 第一个阶段，从左上到右下.
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i > 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                }
+                if (j > 0) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                }
+            }
+        }
+        // 第二个阶段，从右下到左上.
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (i + 1 < n) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j + 1 < n) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+        int res = -1;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                // 寻找水的地方.
+                if (grid[i][j] == 0) {
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+        return res == Integer.MAX_VALUE / 2 ? -1 : res;
+    }
+
     // bfs, 部分case可以获取正确答案，全量case会运行失败，超出时间限制.
     public int maxDistance_dfs(int[][] grid) {
         int m = grid.length;
