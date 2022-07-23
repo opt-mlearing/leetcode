cc34fb4ad3c4bb3f74f24b8a5864664bfcfd19ae
caogl <caogaoli058@163.com>
2022-07-23 22:07:21 +0800

    1319. 连通网络的操作次数

--- src/main/java/leetcode/base/Solution1319.java
+++ src/main/java/leetcode/base/Solution1319.java
@@ -2,14 +2,64 @@ package leetcode.base;
 
 /**
  * 1319. 连通网络的操作次数
- * https://leetcode-cn.com/problems/number-of-operations-to-make-network-connected/
- */
+ * https://leetcode.cn/problems/number-of-operations-to-make-network-connected/
+ *
+ * @author: caogl
+ * @date: 2022/7/23, 22:05
+ **/
 public class Solution1319 {
+
+    public int makeConnected(int n, int[][] connections) {
+        UnionFind unionFind = new UnionFind(n);
+        int size = connections.length;
+        if (size < n - 1) {
+            return -1;
+        }
+        for (int i = 0; i < size; ++i) {
+            int x = connections[i][0];
+            int y = connections[i][1];
+            unionFind.union(x, y);
+        }
+        int res = unionFind.count - 1;
+        return res >= 0 ? res : -1;
+    }
+
+    private static class UnionFind {
+
+        private int[] parent;
+        private int count;
+
+        public UnionFind(int n) {
+            this.count = n;
+            this.parent = new int[n];
+            for (int i = 0; i < n; ++i) {
+                this.parent[i] = i;
+            }
+        }
+
+        public void union(int x, int y) {
+            int px = find(x);
+            int py = find(y);
+            if (px == py) {
+                return;
+            }
+            parent[px] = py;
+            count--;
+        }
+
+        public int find(int x) {
+            while (x != parent[x]) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        }
+    }
 
     private int[] parent;
     private int num;
 
-    public int makeConnected(int n, int[][] connections) {
+    public int makeConnected_1(int n, int[][] connections) {
         parent = new int[n];
         num = n;
         if (connections.length < n - 1) {
